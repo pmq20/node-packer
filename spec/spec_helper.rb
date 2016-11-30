36f820d03dc33d289887e37f519020f54177d05b
Minqi Pan <pmq2001@gmail.com>
2016-11-30 14:06:33 +0800

    remove codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,5 @@
-require 'simplecov'
-SimpleCov.start
-
-if ENV['CI']
-  require 'codecov'
-  require 'certified' if Gem.win_platform?
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
-
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
 require "node/compiler"
 require 'tempfile'
 require 'tmpdir'

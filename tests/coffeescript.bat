62553a702ff8100804680a530384d78ff8f1c169
Minqi Pan <pmq2001@gmail.com>
2017-01-06 11:33:52 +0800

    fixing ci

--- tests/coffeescript.bat
+++
@@ -1,3 +0,0 @@
-git clone --depth 1 https://github.com/jashkenas/coffeescript.git
-cd coffeescript
-ruby ..\bin\nodec bin\coffee
--- tests/nodeclub.bat
+++
@@ -1,3 +0,0 @@
-git clone --depth 1 https://github.com/cnodejs/nodeclub.git
-cd nodeclub
-ruby ..\bin\nodec app.js

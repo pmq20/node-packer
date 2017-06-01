b88a39982842504c0d5cf6aa1420bc66c4c00fa2
Minqi Pan <pmq2001@gmail.com>
2017-06-01 11:05:34 +0800

    test with vs2017

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -94,7 +94,7 @@ class Compiler
   def init_options
     @options[:npm] ||= 'npm'
     @options[:make_args] ||= '-j4'
-    @options[:vcbuild_args] ||= `node -pe process.arch`.to_s.strip
+    @options[:vcbuild_args] ||= "#{`node -pe process.arch`.to_s.strip} vs2017"
     if Gem.win_platform?
       @options[:output] ||= 'a.exe'
     else

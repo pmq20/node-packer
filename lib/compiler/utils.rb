98a2a3dfd38dcbe3fd918ce8a280a102df8f998a
Minqi Pan <pmq2001@gmail.com>
2017-06-15 11:07:40 +0800

    update license

--- lib/compiler/utils.rb
+++ lib/compiler/utils.rb
@@ -1,4 +1,6 @@
 # Copyright (c) 2017 Minqi Pan <pmq2001@gmail.com>
+#                    Yuwei Ba <xiaobayuwei@gmail.com>
+#                    Alessandro Agosto <agosto.alessandro@gmail.com>
 # 
 # This file is part of Node.js Compiler, distributed under the MIT License
 # For full terms see the included LICENSE file
